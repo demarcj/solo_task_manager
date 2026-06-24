ac08fb789bd793ed99a43c5d79d11bbbab3fe7c6
DeMarc Johnson <demarcjohnson@gmail.com>
2026-06-24 03:41:00 -0400

    Added a the ability to save the time for that day

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,25 @@
 declare module '*.css';
+
+interface TemporalInstant {
+  until: (
+    other: TemporalInstant,
+    options: {
+      largestUnit: 'hours';
+      smallestUnit: 'seconds';
+      roundingMode: 'floor';
+    }
+  ) => {
+    hours: number;
+    minutes: number;
+    seconds: number;
+  };
+}
+
+declare const Temporal: {
+  Now: {
+    instant: () => TemporalInstant;
+    plainDateISO: () => {
+      toString: () => string;
+    };
+  };
+};
